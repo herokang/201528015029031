176c66d2e59af1a5a8afe0fbbe949e13f9a59c17
YinKang <whuyinkang@gmail.com>
2015-10-18 18:35:23 +0000

    homework 3

---
+++ rails_hw/app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @sort_by = params[:sort_by]
+    if @sort_by == "release_date"
+      query = Movie.order(:release_date)
+    elsif @sort_by == "title"
+      query = Movie.order(:title)
+    else
+      query = Movie
+    end
+    
+    @all_ratings = Movie.ratings
+    if  !params[:ratings].nil?
+      @ratings = params[:ratings].map { |r| r[0] }
+      @movies = query.where(rating: @ratings)
+    else
+      @movies = query.all
+    end
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
